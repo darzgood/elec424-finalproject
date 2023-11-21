751cd2cf8058879d631c0482a0bb06c9b055ec18
John-Reko <89427856+John-Reko@users.noreply.github.com>
2023-11-21 09:16:43 -0600

    Add files via upload

--- pwm_test.py
+++ pwm_test.py
@@ -3,18 +3,28 @@ from rpi_hardware_pwm import HardwarePWM
 
 # Duty cycle values
 # PWM pins for default Device Tree Overlay is channel0 -> Pin 18
-# channel1 -> Pin 19
-backwards_pwm = 5
-stop_pwm = 7.5
-forward_pwm = 10
-off_pwm = 0
-pwm = HardwarePWM(pwm_channel=0, hz=50)
-pwm.start(off_pwm)
+# channel1 -> Pin 13
 
-def pwm_output(duty_cycle, finish_pwm=False):
-   # pwm = HardwarePWM(pwm_channel=0, hz=50)
-    pwm.stop()
-    pwm.start(duty_cycle)
-    #pwm.change_duty_cyle(10)
+# Constants
+BACK_PWM = 5
+STOP_PWM = 7.5
+FORWARD_PWM = 10
+OFF_PWM = 0
 
-pwm_output(forward_pwm)
+def pwm_init(frequency1, frequency2):
+    pwm0 = HardwarePWM(pwm_channel=0, hz=frequency1)
+    pwm1 = HardwarePWM(pwm_channel=1, hz=frequency2)
+    pwm = (pwm0, pwm1)
+    pwm[0].start(OFF_PWM)
+    pwm[1].start(OFF_PWM)
+    return pwm
+
+def pwm_output(pwm_channels, duty_cycle, channel):
+    # Note that pwm input is a tupe with each pwm channel
+    pwm_channels[channel].stop()
+    pwm_channels[channel].start(duty_cycle)
+    return 0
+
+pwm = pwm_init(20, 1)
+pwm_output(pwm, STOP_PWM, 1)
+pwm_output(pwm, BACK_PWM, 0)
